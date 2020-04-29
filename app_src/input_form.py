b19de84b90a398b0ff241dea26a8dd5370885180
Thomas Murphy <thomasmurphy987@gmail.com>
2020-04-29 13:37:02 +0100

    changed number limit values

--- app_src/input_form.py
+++ app_src/input_form.py
@@ -3,7 +3,7 @@ from wtforms import FloatField, SubmitField
 from wtforms.validators import InputRequired, NumberRange
 
 class InputForm(FlaskForm):
-    afp = FloatField("AFP Value", validators=[InputRequired(message="This field is required"),NumberRange(min=-100.0, max=100.0,message="Value not valid. Must be in range [-100,100]")])
-    afp_l3 = FloatField("AFP-L3 Value", validators=[InputRequired(message="This field is required"),NumberRange(min=-100.0, max=100.0,message="Value not valid. Must be in range [-100,100]")])
-    dcp = FloatField("DCP Value", validators=[InputRequired(message="This field is required"),NumberRange(min=-100.0, max=100.0,message="Value not valid. Must be in range [-100,100]")])
+    afp = FloatField("AFP Value", validators=[InputRequired(message="This field is required"),NumberRange(min=0.0, max=10000.0,message="Value not valid. Must be in range [0,10000]")])
+    afp_l3 = FloatField("AFP-L3 Value", validators=[InputRequired(message="This field is required"),NumberRange(min=0.0, max=10000.0,message="Value not valid. Must be in range [0,10000]")])
+    dcp = FloatField("DCP Value", validators=[InputRequired(message="This field is required"),NumberRange(min=0.0, max=10000.0,message="Value not valid. Must be in range [0,10000]")])
     submit = SubmitField("Predict")
